61cdfa701db05f5cee1139001077ef653235c183
ShinNakajima128 <ukauka224@i.softbank.jp>
2023-10-11 18:53:16 +0900

    [add]画面のフェード機能を追加

---
+++ Assets/Scripts/System/FadeManager.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+/// <summary>
+/// 画面のフェード機能を管理するManagerクラス
+/// </summary>
+public class FadeManager : SingletonMonoBehaviour<FadeManager>
+{
+    #region property
+    protected override bool IsDontDestroyOnLoad => true;
+    #endregion
+
+    #region serialize
+    [Header("Variables")]
+    [Tooltip("フェードにかける時間")]
+    [SerializeField]
+    private float _fadeTime = 1.0f;
+
+    [Header("Objects")]
+    [Tooltip("フェードするImage")]
+    [SerializeField]
+    private Image _fadeImage = default;
+    #endregion
+
+    #region private
+    /// <summary>フェード中かどうか</summary>
+    private bool _isFading = false;
+    #endregion
+
+    #region public method
+    /// <summary>
+    /// 画面を徐々に暗転させる
+    /// </summary>
+    public static void Fade(FadeType type, Action callback = null)
+    {
+        //フェード中の場合は何も行わない
+        if (Instance._isFading)
+        {
+            return;
+        }
+
+        Instance._isFading = true;
+        Instance._fadeImage.raycastTarget = true;
+        float fadeTarget = default;
+
+        //フェードの種類によって処理を変更する
+        switch (type)
+        {
+            //徐々に明転する
+            case FadeType.In:
+                fadeTarget = 0f;
+                if (Instance._fadeImage.color.a < 1.0f)
+                {
+                    Instance._fadeImage.DOFade(1.0f, 0f);
+                }
+                break;
+            //徐々に暗転する
+            case FadeType.Out:
+                fadeTarget = 1.0f;
+                if (Instance._fadeImage.color.a > 0f)
+                {
+                    Instance._fadeImage.DOFade(0f, 0f);
+                }
+                break;
+            default:
+                break;
+        }
+
+        Instance._fadeImage.DOFade(fadeTarget, Instance._fadeTime)
+                           .SetEase(Ease.Linear)
+                           .OnComplete(() =>
+                           {
+                               Instance._isFading = false;
+                               Instance._fadeImage.raycastTarget = false;
+                               //完了後に引数に渡されている処理を実行
+                               callback?.Invoke();
+                           });
+    }
+    #endregion
+}
